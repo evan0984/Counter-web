4acdda91754d9a1d3e971d84d4101a9cc6a7f7fd
Maksym0915 <Davidhayes1015@gmail.com>
2023-11-07 09:54:18 -0600

    Update

---
+++ src/App - Copy.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
+
+class App extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <NavBar/>
+                <main className="container">
+                    <Counters />
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
+
+
